0c18f90b9fa347afa31c64feba238ddd8a4fd82d
dmcgaa <mcgaa.david@gmail.com>
2016-01-08 18:59:27 +0000

    add mongodb client connection, and mongodb dependency in NodeJS

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient,
+  test = require('assert');
+  // Connection url
+// var dbUrl = 'mongodb://'+ process.env.MONGOLAB_USER + ":" + process.env.MONGOLAB_UPW + "@" +"ds039125.mongolab.com:39125/mylonelydb";
+// console.log(process.env.MONGOLAB_USER);
+var userName = "votingapp";
+var userPw = "free123zt";
+var dbUrl = 'mongodb://'+ userName + ":" + userPw + "@" +"ds039125.mongolab.com:39125/mylonelydb";
+console.log(dbUrl);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,4 +24,18 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+// Connect using MongoClient
+MongoClient.connect(dbUrl, function(err, db) {
+  test.equal(null, err);
+  db.close();
+  // // Use the admin database for the operation
+  // var adminDb = db.admin();
+  // // List all the available databases
+  // adminDb.listDatabases(function(err, dbs) {
+  //   test.equal(null, err);
+  //   test.ok(dbs.databases.length > 0);
+  //   db.close();
+  // });
 });
