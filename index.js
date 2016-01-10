33d5ffc199a639f3a42563ed1eab9d55093d4451
dmcgaa <mcgaa.david@gmail.com>
2016-01-10 15:57:52 +0000

    ui elements for mongodb view and add

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient,
   test = require('assert');
 // var myDb = require('./public/js/db.js');
-  
-  
+
+
 // Connection url
 var userName = process.env.MONGOLAB_USER || "null";
 var userPw = process.env.MONGOLAB_UPW || "null";
-var dbUrl = 'mongodb://'+ userName + ":" + userPw + "@" +"ds039125.mongolab.com:39125/mylonelydb";
+var dbUrl = 'mongodb://' + userName + ":" + userPw + "@" + "ds039125.mongolab.com:39125/mylonelydb";
+var mongoTemp = {};
+
 console.log(dbUrl);
+mongoConnect();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -19,6 +22,12 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+// Give Views/Layouts direct access to data.
+app.use(function(req, res, next) {
+  res.locals.mongoTemp = mongoTemp;
+  next();
+});
 
 app.get('/', function(request, response) {
   response.render('pages/index')
@@ -35,31 +44,36 @@ app.listen(app.get('port'), function() {
 });
 
 // Connect using MongoClient
-MongoClient.connect(dbUrl, function(err, db) {
-  test.equal(null, err);
-  //db.close();
-  var testVar = {
-    _id: "myOne",
-    item: "something",
-    qty: 5,
-  }
-  // console.log(db.listCollections());
-  // db.listCollections(); //will print the collection object
-  // db.votingapp.insert(testVar);
-  // db.insert(testVar);
-  
-  var collection = db.collection("votingapp");
-  //insert to collection
-  collection.insert(testVar);
-  
-  //read from collection
-  collection.find({
-    qty: {$gt: 2}
-  }).toArray(function(err, docs){
-    if(err) throw err;
-    console.log(JSON.stringify(docs));
-    db.close();
-  })
-  
-  // db.close();  //if using find, error with sockets closed before db returns (this close happens first, since db find is asynchronous)
-});
+function mongoConnect() {
+  MongoClient.connect(dbUrl, function(err, db) {
+    test.equal(null, err);
+    //db.close();
+    var testVar = {
+        _id: "myOne",
+        item: "something",
+        qty: 5,
+      }
+      // console.log(db.listCollections());
+      // db.listCollections(); //will print the collection object
+      // db.votingapp.insert(testVar);
+      // db.insert(testVar);
+
+    var collection = db.collection("votingapp");
+    //insert to collection
+    collection.insert(testVar);
+
+    //read from collection
+    collection.find({
+      qty: {
+        $gt: 2
+      }
+    }).toArray(function(err, docs) {
+      if (err) throw err;
+      mongoTemp = docs;
+      console.log(JSON.stringify(mongoTemp));
+      db.close();
+    })
+
+    // db.close();  //if using find, error with sockets closed before db returns (this close happens first, since db find is asynchronous)
+  });
+}
