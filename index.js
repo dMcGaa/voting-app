ed06bbf38ba1c2455e2293f865c7dcaa9c093362
dmcgaa <mcgaa.david@gmail.com>
2016-01-12 04:33:54 +0000

    leave mongodb connection open for faster response, let application terminate when done

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var dbUrl = 'mongodb://' + userName + ":" + userPw + "@" + "ds039125.mongolab.co
 var mongoTemp = {};
 
 console.log(dbUrl);
+var dbConn;
 mongoConnect();
 
 app.set('port', (process.env.PORT || 5000));
@@ -46,7 +47,11 @@ app.post("/loadDatabase", function(req, res){
   // mongoConnectFind(response);
   // res.send("Added");
   console.log("load database was clicked");
-  mongoConnectFind2(function(){
+  // mongoConnectFind2(function(){
+  //   console.log("done loading");
+  //   res.send(mongoTemp);
+  // });
+  mongoFind(function(){
     console.log("done loading");
     res.send(mongoTemp);
   });
@@ -76,34 +81,8 @@ app.listen(app.get('port'), function() {
 function mongoConnect() {
   MongoClient.connect(dbUrl, function(err, db) {
     test.equal(null, err);
-    //db.close();
-    var testVar = {
-        _id: "myOne",
-        item: "something",
-        qty: 5,
-      }
-      // console.log(db.listCollections());
-      // db.listCollections(); //will print the collection object
-      // db.votingapp.insert(testVar);
-      // db.insert(testVar);
-
-    var collection = db.collection("votingapp");
-    //insert to collection
-    collection.insert(testVar);
-
-    //read from collection
-    collection.find({
-      qty: {
-        $gt: 2
-      }
-    }).toArray(function(err, docs) {
-      if (err) throw err;
-      mongoTemp = docs;
-      console.log(JSON.stringify(mongoTemp));
-      db.close();
-    })
-
-    // db.close();  //if using find, error with sockets closed before db returns (this close happens first, since db find is asynchronous)
+    dbConn = db;  //mongodb connection instance
+    // db.close();  //no need to close, let application termination handle this
   });
 }
 
@@ -144,6 +123,21 @@ function mongoConnectFind2(callback) {
       callback();//callback once response is obtained (Asynchronous)
     })
   });
+}
+
+function mongoFind(callback) {
+    var collection = dbConn.collection("votingapp");
+    //read from collection
+    collection.find({
+      qty: {
+        $gt: 2
+      }
+    }).toArray(function(err, docs) {
+      if (err) throw err;
+      mongoTemp = docs;
+      console.log(JSON.stringify(mongoTemp));
+      callback();//callback once response is obtained (Asynchronous)
+    })
 }
 
 // Connect using MongoClient and add to database
