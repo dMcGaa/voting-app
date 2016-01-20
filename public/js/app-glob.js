51d43dfb1813392fd68c3ff5db96a62c1a649dfe
dmcgaa <mcgaa.david@gmail.com>
2016-01-20 16:04:19 +0000

    login/logout cookie function implemented on login page

---
+++ public/js/app-glob.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var user = checkCookie();
+    if (user !==""){
+        $("#header-user").html(user);
+        $("#header-user").css("display", "block");
+        $("#header-logout").css("display", "block");
+        $("#header-login").css("display", "none");
+        $("#header-signup").css("display", "none");
+    }
+    // $("#header-logout").onclick(function(e){
+    //     e.preventDefault();
+        document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC"; //previous date will cause cookie to delete
+        // window.location.href = "index"; //simulate a link click  
+    // })
+    
+})
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        return user;
+        // alert("Welcome again " + user);
+    }
+    else{
+        return "";
+    }
+}
